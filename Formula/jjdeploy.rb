99e22189a2b8fb27c524d4f01455fbb184d14309
José Manuel Sánchez <buscarini@gmail.com>
2014-11-04 11:04:49 +0100

    Removed dependency

--- Formula/jjdeploy.rb
+++ Formula/jjdeploy.rb
@@ -4,8 +4,6 @@ class Jjdeploy < Formula
   homepage 'https://github.com/buscarini/jjdeploy'
   url 'https://github.com/buscarini/jjdeploy/archive/0.0.5.tar.gz'
   sha1 'd529f7dbe9db4cd8dfa4d99156316055a344c11f'
-
-  depends_on 'Xcode' => :recommended
   
   def install
     prefix.install 'jjdeploy_resources','jjdeploy.config'
