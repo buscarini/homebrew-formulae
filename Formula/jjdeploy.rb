6ca27d8309c96822c04cb8a0a14553018fdef2ad
José Manuel Sánchez <buscarini@gmail.com>
2014-11-04 10:57:23 +0100

    Fixed sha

--- Formula/jjdeploy.rb
+++ Formula/jjdeploy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jjdeploy < Formula
   homepage 'https://github.com/buscarini/jjdeploy'
   url 'https://github.com/buscarini/jjdeploy/archive/0.0.5.tar.gz'
-  sha1 'd529f7dbe9db4cd8dfa4d99156316055a344c11f'
+  sha1 'c87cda5ae460e531fad78427c61e8bd964983814'
 
   depends_on 'Xcode' => :recommended
   
