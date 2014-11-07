454590eb0b829d1821c0dee4965117eaa7d99c90
José Manuel Sánchez <buscarini@gmail.com>
2014-11-07 13:45:31 +0100

    Now you can pass only a version number to the sha script

--- sha.sh
+++ sha.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-curl -fsSL $1 > jjdeploy
+if [ $# -lt 1 ];
+then
+	echo "Usage: sha.sh jjdeploy_version | url (Example: sha.sh 0.2.2)"
+	exit 1
+fi
+
+url=$1
+if [[ $url != https://* ]];
+then
+url="https://github.com/buscarini/jjdeploy/archive/"$url".tar.gz"
+fi
+
+curl -fsSL $url > jjdeploy
 shasum jjdeploy
 rm jjdeploy
