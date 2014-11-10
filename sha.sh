11a4ed7793264e122ac3b08e076243e47b5902b4
José Manuel Sánchez <buscarini@gmail.com>
2014-11-10 12:55:39 +0100

    0.2.3. Now sha script copies the result to the pasteboard

--- sha.sh
+++ sha.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ];
 then
-	echo "Usage: sha.sh jjdeploy_version | url (Example: sha.sh 0.2.2)"
+	echo "Usage: sha.sh jjdeploy_version | url (Example: sha.sh 0.2.3)"
 	exit 1
 fi
 
@@ -13,5 +13,7 @@ url="https://github.com/buscarini/jjdeploy/archive/"$url".tar.gz"
 fi
 
 curl -fsSL $url > jjdeploy
-shasum jjdeploy
+sha="$(shasum README.md | awk '{print $1}' | sed -e 's/^ *//' -e 's/ *$//' )"
+echo -n $sha | pbcopy
+echo "Copied sha to pasteboard"
 rm jjdeploy
