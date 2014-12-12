4273c8ad8b398fe1ecfd95b18c6277ae7963fed3
José Manuel Sánchez <buscarini@gmail.com>
2014-12-12 09:08:40 +0100

    jjdeploy 0.3.6

--- Formula/jjdeploy.rb
+++ Formula/jjdeploy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jjdeploy < Formula
   homepage 'https://github.com/buscarini/jjdeploy'
-  url 'https://github.com/buscarini/jjdeploy/archive/0.3.5.tar.gz'
-  sha1 'd4f5cb39cf632a9d872e42eea05d457e8afa808f'
+  url 'https://github.com/buscarini/jjdeploy/archive/0.3.6.tar.gz'
+  sha1 '8ddb6d833c43f6bb8f44ad5c5d939b0eedde18f1'
   
   def install
     prefix.install 'jjdeploy_resources','jjdeploy.config'
