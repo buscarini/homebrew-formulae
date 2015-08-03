149b836d9850458b012926e1680a441c39f8f77f
José Manuel Sánchez <buscarini@gmail.com>
2015-08-03 11:39:03 +0200

    jjdeploy 0.7.0

--- Formula/jjdeploy.rb
+++ Formula/jjdeploy.rb
@@ -2,13 +2,15 @@ require 'formula'
 
 class Jjdeploy < Formula
   homepage 'https://github.com/buscarini/jjdeploy'
-  url 'https://github.com/buscarini/jjdeploy/archive/0.6.2.tar.gz'
-  sha1 'ed83113925e7c0b77a766a00a98d0269d10608ad'
+  url 'https://github.com/buscarini/jjdeploy/archive/0.7.0.tar.gz'
+  sha1 'c97d7a12e3e7462023a91222a84597106914e72a'
   
   def install
     prefix.install 'jjdeploy_resources','jjdeploy.config'
 
     bin.install 'jjdeploy'
+    bin.install 'upload.py'
+    bin.install 'askpass.py'
   end
 
   test do
