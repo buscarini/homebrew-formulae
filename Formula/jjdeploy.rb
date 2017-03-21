b38bfd5ccd81242c778a5f5d3855d0440d5db5c6
Jenkins <jenkins@Mac-mini-Treenovum.local>
2017-03-21 14:36:17 +0100

    jjdeploy 0.8.8

--- Formula/jjdeploy.rb
+++ Formula/jjdeploy.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class Jjdeploy < Formula
   homepage 'https://github.com/buscarini/jjdeploy'
-  url 'https://github.com/buscarini/jjdeploy/archive/0.8.7.tar.gz'
-  sha256 'e93fea3156ab6d30ffc0a1c7fd6b72848332a3d4ab03aed6dd7347dcc424cd1d'
+  url 'https://github.com/buscarini/jjdeploy/archive/0.8.8.tar.gz'
+  sha256 'b09f50327b9160fe42b1ecd0a714853cdf0aa45dfdd1d3c67643759a9298f42b'
   
   def install
     prefix.install 'jjdeploy_resources','jjdeploy.config'
 
-    bin.install 'jjdeploy'
-    bin.install 'upload.py'
-    bin.install 'askpass.py'
-    bin.install 'xcbuildsafe.sh'
+    bin.install 'bin/jjdeploy'
+    bin.install 'bin/upload.py'
+    bin.install 'bin/askpass.py'
+    bin.install 'bin/xcbuildsafe.sh'
   end
 
   test do
