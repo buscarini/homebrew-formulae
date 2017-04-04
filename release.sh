b9a3184360a21777fcd54a1ee732e1259f1d525d
José Manuel <buscarini@gmail.com>
2017-04-04 11:52:43 +0200

    Added script to simplify releasing new versions

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Update the Formula for a new version.
+#
+# Usage: ./release VERSION
+#
+# Requires sha1sum be installed.
+#
+###
+set -e
+
+if [ -z "$1" ]; then
+  echo "usage: ./release VERSION" >&2
+  exit 64
+fi
+
+version=$1
+formula=Formula/jjdeploy.rb
+
+sha256=$(curl -sL "https://github.com/buscarini/jjdeploy/archive/$version.tar.gz" | shasum -a256 | cut -d " " -f 1)
+
+git checkout master
+git pull
+
+sed 's/^\(.*\/v\).*\(.tar.gz"\)$/\1'"$version"'\2/' "$formula" > "$formula.modified"
+sed 's/^\(.*sha256 "\).*\(".*\)$/\1'"$sha256"'\2/' "$formula.modified" > "$formula"
+rm "$formula.modified"
+
+git add "$formula"
+git commit -m "Release v $version"
+git show
+git push
